074d45679691b4930de7e22cadcba6e0c4fcfa78
Dana Bottoni <danabottoni@gmail.com>
2021-10-04 11:10:40 -0400

    create nav and about sections

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+// import Portfolio from './components/Portfolio';
+// import ContactForm from './components/Contact';
+// import Resume from './components/Resume';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  
+    return (
+      <div>
+          <Nav/>
+        <main>
+          <About />
+        </main>
+      </div>
+    );
+  }
+  
+  // return (
+  //   <div className="App">
+  //     <Nav></Nav>
+  //     <main>
+  //       <About></About>
+  //       <Portfolio></Portfolio>
+  //       <ContactForm></ContactForm>
+  //       <Resume></Resume>
+  //     </main>
+  //   </div>
+  // );
+  //}
 
 export default App;
