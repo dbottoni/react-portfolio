66048705a684a4d91cdea5e00f19ff7a080b83a7
Dana Bottoni <danabottoni@gmail.com>
2021-10-07 16:30:30 -0400

    resume page changes

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,20 +4,21 @@ import PDF from '../../assets/Resume.pdf';
 
 function Resume(){
  return(
-  <div>
-    <a href={PDF} download="Resume_Bottoni.pdf">Download My Resume Here</a>
-  <ul>
-    <li>Skill Set</li>
-    <li>HTML5</li>
-    <li>CSS</li>
-    <li>React</li>
-    <li>JavaScript</li>
-    <li>Sequelize</li>
-    <li>SQL</li>
-    <li>Git, GitHub</li>
-    <li>Express</li>
-    <li>Node</li>
-  </ul>
+  <div className="text-center">
+      <a href={PDF} download="Resume_Bottoni.pdf">Download My Resume Here</a>
+      <p></p>
+    <ul>
+      <li>Skill Set</li>
+      <li>HTML5</li>
+      <li>CSS</li>
+      <li>React</li>
+      <li>JavaScript</li>
+      <li>Sequelize</li>
+      <li>SQL</li>
+      <li>Git, GitHub</li>
+      <li>Express</li>
+      <li>Node</li>
+    </ul>
   </div>
  )
 }
